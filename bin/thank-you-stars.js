7046103b9d255020bb8d571cbbbab356e09c13b8
Sam-Mauris Yong <mauris@hotmail.sg>
2017-11-08 15:05:41 +0000

    update the bin to use env variable for github token

--- bin/thank-you-stars.js
+++ bin/thank-you-stars.js
@@ -7,15 +7,12 @@ const fs = require('fs');
 const loadJson = require('../lib/loadJson');
 const thankYou = require('../');
 
-const configPath = path.join(getUserHome(), '.thank-you-stars.json');
-if (!fs.existsSync(configPath)) {
-  console.error(`Save your token as ${configPath}`);
-  process.exit(1);
-}
-const config = loadJson(configPath);
+const config = {
+  token: process.env.GITHUB_TOKEN,
+};
 
 if (!config.token) {
-  console.error('`token` property is not found in .thank-you-stars.json');
+  console.error('You need to set your Github Token as a environment variable `GITHUB_TOKEN`.');
   process.exit(1);
 }
 
@@ -27,7 +24,3 @@ const pkgPath = path.join(process.cwd(), 'package.json');
 const pkgJson = loadJson(pkgPath);
 
 thankYou(pkgJson, config);
-
-function getUserHome() {
-  return process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
-}
