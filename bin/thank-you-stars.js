274d8603f8f4ff8afccde6a0de25d7a3988f9781
Sam-Mauris Yong <mauris@hotmail.sg>
2017-11-09 09:51:13 +0000

    update config to load from file, then check for env var

--- bin/thank-you-stars.js
+++ bin/thank-you-stars.js
@@ -7,12 +7,19 @@ const fs = require('fs');
 const loadJson = require('../lib/loadJson');
 const thankYou = require('../');
 
-const config = {
-  token: process.env.GITHUB_TOKEN,
-};
+const configPath = path.join(getUserHome(), '.thank-you-stars.json');
+
+let config = {};
+if (fs.existsSync(configPath)) {
+  config = loadJson(configPath);
+}
+
+// check if the token is set, otherwise we try taking it from env var.
+config.token = config.token || process.env.GITHUB_TOKEN;
 
 if (!config.token) {
-  console.error('You need to set your Github Token as a environment variable `GITHUB_TOKEN`.');
+  console.error(`You need to either store your Github token to ${configPath} `
+     + 'or set your Github Token as a environment variable \'GITHUB_TOKEN\'.');
   process.exit(1);
 }
 
@@ -24,3 +31,7 @@ const pkgPath = path.join(process.cwd(), 'package.json');
 const pkgJson = loadJson(pkgPath);
 
 thankYou(pkgJson, config);
+
+function getUserHome() {
+  return process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
+}
